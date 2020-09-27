95b4b23d7650afae6b83ba1eb3bbc95d23f48e1c
David P <pipeyodavid@hotmail.com>
2020-09-27 19:08:02 +0200

    main menu script

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+public void PlayGame()
+    {
+        SceneManager.LoadScene("FelipeScene");
+        //SceneManager.GetActiveScene().buildIndex;
+    }
+
+    public void Credits()
+    {
+
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+}
