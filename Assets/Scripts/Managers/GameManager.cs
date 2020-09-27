62ad89389a7851ae3cfa1838105e4aa9a7c87dc8
DavyMarquez <davidmarquezcalavia@gmail.com>
2020-09-27 20:47:34 +0200

    game manager

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public GameObject player;
+
+    private bool notifiedDead = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // play music start here?
+        notifiedDead = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        if(!notifiedDead && player.GetComponent<Animator>().GetBool("IsDying"))
+        {
+            
+            OnPlayerDeath();
+        }
+    }
+
+
+    void OnPlayerDeath()
+    {
+        Debug.Log("muerte");
+        notifiedDead = true;
+        // play death music
+    }
+
+}
